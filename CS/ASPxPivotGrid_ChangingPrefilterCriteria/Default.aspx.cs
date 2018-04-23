16e65994124823472db73398a5e8d23db12102ea
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ASPxPivotGrid_ChangingPrefilterCriteria/Default.aspx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web.UI;
+
+namespace ChangingPrefilterCriteria {
+    public partial class _Default : Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            ASPxPivotGrid1.Prefilter.CriteriaString = "[" + fieldCountry.PrefilterColumnName + 
+                "] == 'UK' OR [" + fieldCountry.PrefilterColumnName + "] == 'USA'";
+        }
+    }
+}
---
+++ VB/ASPxPivotGrid_ChangingPrefilterCriteria/Default.aspx.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Web.UI
+
+Namespace ChangingPrefilterCriteria
+	Partial Public Class _Default
+		Inherits Page
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+			ASPxPivotGrid1.Prefilter.CriteriaString = "[" & fieldCountry.PrefilterColumnName & "] == 'UK' OR [" & fieldCountry.PrefilterColumnName & "] == 'USA'"
+		End Sub
+	End Class
+End Namespace
